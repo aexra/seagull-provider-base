4b921a69a67d11cf79d7548708df2a3e54efd6f8
Nikita Fomin <defdXs@yandex.ru>
2025-05-17 16:39:55 +0300

    Some controllers

---
+++ Seagull/Seagull.API/Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Seagull.API.DTO.auth.Request;
+using Seagull.API.DTO.auth.Response;
+using Seagull.API.Services;
+using Seagull.Core.Entities.Identity;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Seagull.API.Controllers;
+
+[Route("api/auth")]
+[ApiController]
+public class AuthController(
+    UserManager<User> userManager,
+    TokenService tokenService,
+    IConfiguration config) : ControllerBase
+{
+    private readonly UserManager<User> _userManager = userManager;
+    private readonly TokenService _tokenService = tokenService;
+    private readonly IConfiguration _config = config;
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AuthResponse>> Register([FromBody] RegisterDto dto)
+    {
+        var user = new User
+        {
+            Email = dto.Email, // уникальный, можно поменять, но сложно
+            UserName = dto.UserName, // уникальный, нельзя менять
+            DisplayName = dto.DisplayName, // не уникальный, можно поменять
+            Tag = dto.DisplayName, // уникальный, можно поменять
+        };
+
+        var result = await _userManager.CreateAsync(user, dto.Password);
+        if (!result.Succeeded)
+            return BadRequest(result.Errors);
+
+        return Ok(await GenerateAuthResponse(user));
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AuthResponse>> Login([FromBody] LoginDto dto)
+    {
+        // Ищем по email ИЛИ username
+        var user = await _userManager.FindByNameAsync(dto.Login)
+                 ?? await _userManager.FindByEmailAsync(dto.Login);
+
+        if (user == null || !await _userManager.CheckPasswordAsync(user, dto.Password))
+            return Unauthorized("Invalid login or password");
+
+        await _userManager.UpdateAsync(user);
+        return Ok(await GenerateAuthResponse(user));
+    }
+
+    [HttpPost("refresh")]
+    public async Task<ActionResult<AuthResponse>> Refresh([FromBody] RefreshTokenDto dto)
+    {
+        var principal = _tokenService.GetPrincipalFromExpiredToken(dto.AccessToken);
+        var username = principal?.FindFirst(JwtRegisteredClaimNames.Nickname)?.Value;
+
+        if (string.IsNullOrEmpty(username)) return Unauthorized();
+
+        var user = await _userManager.FindByNameAsync(username);
+        if (user == null) return Unauthorized();
+
+        return Ok(await GenerateAuthResponse(user));
+    }
+
+    private async Task<AuthResponse> GenerateAuthResponse(User user)
+    {
+        var roles = await _userManager.GetRolesAsync(user);
+        return new AuthResponse(
+            AccessToken: _tokenService.GenerateAccessToken(user, roles),
+            RefreshToken: _tokenService.GenerateRefreshToken()
+        );
+    }
+}
---
+++ Seagull/Seagull.API/Controllers/UserController.cs
@@ -0,0 +1,409 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Seagull.API.DTO.auth.Response;
+using Seagull.API.Extensions;
+using Seagull.Core.Entities.Identity;
+using Seagull.Infrastructure.Hooks;
+using Seagull.Infrastructure.Services;
+using System.Security.Claims;
+
+namespace Seagull.API.Controllers;
+
+[Route("api/user")]
+[ApiController]
+public class UserController(
+UserManager<User> userManager,
+S3Hook hook,
+S3Service s3) : ControllerBase
+{
+    private readonly UserManager<User> _userManager = userManager;
+    private readonly S3Service _s3 = s3;
+    private readonly S3Hook _hook = hook;
+
+    private const string _bucketName = "user";
+
+    [HttpGet("s")]
+    public async Task<IActionResult> SearchUsers(
+        [FromQuery] string? username,
+        [FromQuery] string? tag,
+        [FromQuery] string? displayname,
+        [FromQuery] string? id,
+        [FromQuery] string? email,
+        [FromQuery] int page = 1,
+        [FromQuery] int size = 10
+    )
+    {
+        if ((username == null || username.Length < 3)
+            && (tag == null || tag.Length < 3)
+            && (displayname == null || displayname.Length < 3)
+            && (id == null || id.Length < 5)
+            && (email == null || email.Length < 3)) return Ok(new List<string>());
+
+        if (id != null)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+        if (email != null)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.Email == email);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+        if (username != null) return Ok(
+            await _userManager.Users
+                .Where(u => u.UserName!.ToLower().StartsWith(username.ToLower()))
+                .Skip((page - 1) * size)
+                .Take(size)
+                .ToListAsync()
+        );
+        if (tag != null) return Ok(
+            await _userManager.Users
+                .Where(u => u.Tag.ToLower().StartsWith(tag.ToLower()))
+                .Skip((page - 1) * size)
+                .Take(size)
+                .ToListAsync()
+        );
+        if (displayname != null) return Ok(
+            await _userManager.Users
+                .Where(u => u.DisplayName.ToLower().StartsWith(displayname.ToLower()))
+                .Skip((page - 1) * size)
+                .Take(size)
+                .ToListAsync()
+        );
+
+        return BadRequest();
+    }
+
+    [HttpGet("me")]
+    [Authorize]
+    public async Task<ActionResult<UserProfileResponse>> GetMyProfile()
+    {
+        var user = await this.CurrentUserAsync(_userManager);
+        if (user == null) return Unauthorized();
+
+        var roles = await _userManager.GetRolesAsync(user);
+
+        return new UserProfileResponse(
+            Id: user.Id,
+            Email: user.Email!,
+            UserName: user.UserName!,
+            DisplayName: user.UserName!,
+            Tag: user.Tag,
+            AvatarUrl: user.AvatarUrl,
+            BannerUrl: user.BannerUrl,
+            BannerColor: user.BannerColor,
+            Roles: roles
+        );
+    }
+
+    [HttpGet("{userId}")]
+    public async Task<ActionResult<UserProfileResponse>> GetProfile([FromRoute] string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+            return NotFound("User not found");
+
+        var roles = await _userManager.GetRolesAsync(user);
+
+        return new UserProfileResponse(
+            Id: user.Id,
+            Email: user.Email!,
+            UserName: user.UserName!,
+            DisplayName: user.UserName!,
+            Tag: user.Tag,
+            AvatarUrl: user.AvatarUrl,
+            BannerUrl: user.BannerUrl,
+            BannerColor: user.BannerColor,
+            Roles: roles
+        );
+    }
+
+    #region Avatars
+
+    [HttpPost("avatar")]
+    [Authorize]
+    public async Task<IActionResult> UploadAvatar(IFormFile file)
+    {
+        var user = await this.CurrentUserAsync(_userManager);
+        if (user == null) return Unauthorized();
+
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file uploaded");
+        }
+
+        if (!file.ContentType.StartsWith("image/"))
+        {
+            return BadRequest("Only image files are allowed");
+        }
+
+        if (!string.IsNullOrEmpty(user.AvatarUrl))
+        {
+            await _s3.DeleteObjectAsync(_bucketName, $"avatar/{user.AvatarUrl}");
+        }
+
+        var result = await _hook.UploadAsync(_bucketName, "avatar", file);
+
+        if (result.Success)
+        {
+            user.AvatarUrl = result.FileName;
+            await _userManager.UpdateAsync(user);
+            return Ok(user);
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    [HttpGet("avatar")]
+    [Authorize]
+    public async Task<IActionResult> GetMyAvatar()
+    {
+        var user = await this.CurrentUserAsync(_userManager);
+        if (user == null) return Unauthorized();
+
+        if (user.AvatarUrl == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _hook.LoadAsync(_bucketName, "avatar", user.AvatarUrl);
+
+        if (result.Success && result.Data != null)
+        {
+            return File(result.Data, $"image/{Path.GetExtension(user.AvatarUrl).ToLower()[1..]}");
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    [HttpGet("{userId}/avatar")]
+    public async Task<IActionResult> GetAvatar([FromRoute] string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+
+        if (user == null || user.AvatarUrl == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _hook.LoadAsync(_bucketName, "avatar", user.AvatarUrl);
+
+        if (result.Success && result.Data != null)
+        {
+            return File(result.Data, $"image/{Path.GetExtension(user.AvatarUrl).ToLower()[1..]}");
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    [HttpDelete("avatar")]
+    [Authorize]
+    public async Task<IActionResult> DeleteMyAvatar()
+    {
+        var user = await this.CurrentUserAsync(_userManager);
+        if (user == null) return Unauthorized();
+
+        if (string.IsNullOrEmpty(user.AvatarUrl))
+        {
+            return NotFound("Avatar not found");
+        }
+
+        var result = await _s3.DeleteObjectAsync(_bucketName, $"avatar/{user.AvatarUrl}");
+
+        if (result.Success)
+        {
+            user.AvatarUrl = null;
+            await _userManager.UpdateAsync(user);
+            return Ok();
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    [HttpDelete("{userId}/avatar")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> DeleteUserAvatar(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null || string.IsNullOrEmpty(user.AvatarUrl))
+        {
+            return NotFound("Avatar not found");
+        }
+
+        var result = await _s3.DeleteObjectAsync(_bucketName, $"avatar/{user.AvatarUrl}");
+
+        if (result.Success)
+        {
+            user.AvatarUrl = null;
+            await _userManager.UpdateAsync(user);
+            return Ok();
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    #endregion
+
+    #region Banners
+
+    [HttpPost("banner")]
+    [Authorize]
+    public async Task<IActionResult> UploadBanner(IFormFile file)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (string.IsNullOrEmpty(userId))
+        {
+            return Unauthorized();
+        }
+
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file uploaded");
+        }
+
+        if (!file.ContentType.StartsWith("image/"))
+        {
+            return BadRequest("Only image files are allowed");
+        }
+
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            return NotFound("User not found");
+        }
+
+        if (!string.IsNullOrEmpty(user.BannerUrl))
+        {
+            await _s3.DeleteObjectAsync(_bucketName, $"banner/{user.BannerUrl}");
+        }
+
+        var result = await _hook.UploadAsync(_bucketName, "banner", file);
+
+        if (result.Success)
+        {
+            user.BannerUrl = result.FileName;
+            await _userManager.UpdateAsync(user);
+            return Ok(user);
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    [HttpGet("banner")]
+    [Authorize]
+    public async Task<IActionResult> GetMyBanner()
+    {
+        var user = await this.CurrentUserAsync(_userManager);
+        if (user == null) return Unauthorized();
+
+        if (user.BannerUrl == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _hook.LoadAsync(_bucketName, "banner", user.BannerUrl);
+
+        if (result.Success && result.Data != null)
+        {
+            return File(result.Data, $"image/{Path.GetExtension(user.BannerUrl).ToLower()[1..]}");
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    [HttpGet("{userId}/banner")]
+    public async Task<IActionResult> GetBanner([FromRoute] string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+
+        if (user == null || user.BannerUrl == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _hook.LoadAsync(_bucketName, "banner", user.BannerUrl);
+
+        if (result.Success && result.Data != null)
+        {
+            return File(result.Data, $"image/{Path.GetExtension(user.BannerUrl).ToLower()[1..]}");
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    [HttpDelete("banner")]
+    [Authorize]
+    public async Task<IActionResult> DeleteMyBanner()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (string.IsNullOrEmpty(userId))
+        {
+            return Unauthorized();
+        }
+
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null || string.IsNullOrEmpty(user.BannerUrl))
+        {
+            return NotFound("Avatar not found");
+        }
+
+        var result = await _s3.DeleteObjectAsync(_bucketName, $"banner/{user.BannerUrl}");
+
+        if (result.Success)
+        {
+            user.BannerUrl = null;
+            await _userManager.UpdateAsync(user);
+            return Ok();
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    [HttpDelete("{userId}/banner")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> DeleteUserBanner(string userId)
+    {
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null || string.IsNullOrEmpty(user.BannerUrl))
+        {
+            return NotFound("Avatar not found");
+        }
+
+        var result = await _s3.DeleteObjectAsync(_bucketName, $"banner/{user.BannerUrl}");
+
+        if (result.Success)
+        {
+            user.BannerUrl = null;
+            await _userManager.UpdateAsync(user);
+            return Ok();
+        }
+        else
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+    }
+
+    #endregion
+}
